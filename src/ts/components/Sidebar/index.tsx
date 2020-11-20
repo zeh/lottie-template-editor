d32d70728ca5d352e27be86f96b3db400787536e
Zeh Fernando <zeh@zehfernando.com>
2020-11-19 22:59:43 -0500

    Sidebar: display field data on side

--- src/ts/components/Sidebar/index.tsx
+++ src/ts/components/Sidebar/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
+import { useMemo } from "react";
 import cx from "classnames";
 
 import Text from "components/Text";
+import { FieldTypes, IFieldImage, IFieldShape, IFieldText, extractFields } from "utils/AnimationUtils";
 
 import * as s from "./styles.scss";
 
@@ -10,14 +12,73 @@ interface IProps {
 	className?: string;
 }
 
-const Sidebar = ({ className }: IProps): JSX.Element => {
+const colorToHex = (c: number[]): string => {
+	return (
+		"#" +
+		c
+			.slice(0, 3)
+			.map((c) => ("00" + Math.round(c * 255).toString(16)).substr(-2, 2))
+			.join("")
+	);
+};
+
+const FieldTextCell = ({ field }: { field: IFieldText }): JSX.Element => {
+	return (
+		<div className={s.fieldCell}>
+			<div className={s.fieldTitle}>{"Text"}</div>
+			<div className={s.fieldName}>{field.name}</div>
+			<div className={s.fieldValue}>{field.value}</div>
+			<div className={s.fieldValue}>{colorToHex(field.color)}</div>
+		</div>
+	);
+};
+
+const FieldImageCell = ({ field }: { field: IFieldImage }): JSX.Element => {
+	return (
+		<div className={s.fieldCell}>
+			<div className={s.fieldTitle}>{"Image"}</div>
+			<div className={s.fieldName}>{field.name}</div>
+			<div className={s.fieldDetail}>{`${field.width}x${field.height}px`}</div>
+		</div>
+	);
+};
+
+const FieldShapeCell = ({ field }: { field: IFieldShape }): JSX.Element => {
+	return (
+		<div className={s.fieldCell}>
+			<div className={s.fieldTitle}>{"Shape"}</div>
+			<div className={s.fieldName}>{field.name}</div>
+			<div className={s.fieldValue}>{colorToHex(field.color)}</div>
+		</div>
+	);
+};
+
+const Sidebar = ({ animation, className }: IProps): JSX.Element => {
+	const fields = useMemo(() => extractFields(animation), [animation]);
+
 	return (
 		<div className={cx(className, s.main)}>
-			<Text.H1>H1</Text.H1>
-			<Text.H2>H2</Text.H2>
-			<Text.H3>H3</Text.H3>
-			<Text.H4>H4</Text.H4>
-			<Text.P>This is a paragraph.</Text.P>
+			{animation ? (
+				<div className={s.fields}>
+					<div className={s.fieldsContainer}>
+						{fields.map((f, i) => {
+							if (f.type === FieldTypes.Text) {
+								return <FieldTextCell key={i} field={f}></FieldTextCell>;
+							} else if (f.type === FieldTypes.Image) {
+								return <FieldImageCell key={i} field={f}></FieldImageCell>;
+							} else if (f.type === FieldTypes.Shape) {
+								return <FieldShapeCell key={i} field={f}></FieldShapeCell>;
+							} else {
+								return null;
+							}
+						})}
+					</div>
+				</div>
+			) : (
+				<div className={s.placeholderMessage}>
+					<Text.P>{"No elements found"}</Text.P>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/ts/components/Sidebar/styles.scss.d.ts
+++ src/ts/components/Sidebar/styles.scss.d.ts
@@ -1,2 +1,10 @@
 // This file is generated automatically; do not edit
 export const main: string;
+export const fields: string;
+export const fieldsContainer: string;
+export const fieldCell: string;
+export const fieldTitle: string;
+export const fieldDetail: string;
+export const fieldName: string;
+export const fieldValue: string;
+export const placeholderMessage: string;
